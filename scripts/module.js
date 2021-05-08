e8972290c7c6318a85dad1ecad14827aed635b9e
Michael Enion <caewok_2000@yahoo.com>
2021-05-08 11:24:02 -0700

    Setup df hotkeys library.

--- scripts/module.js
+++ scripts/module.js
@@ -16,27 +16,32 @@ export function log(...args) {
   } catch (e) {}
 }
 
-
+// https://discord.com/channels/732325252788387980/754127569246355477/819710580784234506
+// init is called almost immediately after the page loads. 
+// At this point, the game global exists, but hasn't yet been initialized, 
+// but all of the core foundry code has been loaded.
 Hooks.once('init', async function() {
   log("Initializing Elevation Ruler Options.");
-  registerSettings();
-  registerHotkeys();
   
 });
 
-
-Hooks.once('ready', async function() {
-  log("Readying Elevation Ruler.");
-  if(!game.modules.get('lib-wrapper')?.active && game.user.isGM) ui.notifications.error("Module Elevation Ruler requires the 'libWrapper' module. Please install and activate it.");
-        
-  if (!game.modules.get('lib-df-hotkeys')?.active && game.user.isGM) ui.notifications.error("'My Module' requires the 'Library: DF Hotkeys' module. Please install and activate this dependency.");
-		
-        
-});
-
+// setup is after init; before ready. 
+// setup is called after settings and localization have been initialized, 
+// but before entities, packs, UI, canvas, etc. has been initialized
 Hooks.once('setup', async function() {
-  log("Setup for Elevation Ruler.");
+  log("Setup.");
+  if(!game.modules.get('lib-wrapper')?.active && game.user.isGM) ui.notifications.error("Module Elevation Ruler requires the 'libWrapper' module. Please install and activate it.");
+  if(!game.modules.get('lib-df-hotkeys')?.active && game.user.isGM) ui.notifications.error("'My Module' requires the 'Library: DF Hotkeys' module. Please install and activate this dependency.");
+
   registerRuler();
-  registerHotkeys();
+  registerHotkeys(); // should go before registering settings, so hotkey group is defined
+  registerSettings();
 });
+
+// modules ready
+// ready is called once everything is loaded up and ready to go.
+Hooks.once('ready', async function() {
+  log("Readying.");
+});
+
 
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
-import { MODULE_ID } from "./module.js";
+import { MODULE_ID, log } from "./module.js";
 
 export function getSetting(settingName) {
   return game.settings.get(MODULE_ID, settingName);
@@ -6,6 +6,7 @@ export function getSetting(settingName) {
 
 
 export function registerSettings() {
+  log("Registering measurement setting.");
   game.settings.register(MODULE_ID, 'measurement-method', {
     name: 'Measurement method',
     hint: 
@@ -24,39 +25,47 @@ export function registerSettings() {
     },
     default: 'CityBlock',
   });
-  
+ 
+  log("Registering hotkey menu.");
   game.settings.registerMenu(MODULE_ID, {
-	  name: 'Elevation Ruler Hotkeys'
+	  name: 'Elevation Ruler Hotkeys',
 	  type: Hotkeys.createConfig('Elevation Ruler Hotkeys', [`${MODULE_ID}.change-elevation-group`]),
 	});
+
+  log("Done registering settings.");
 
 }
 
 export function registerHotkeys() {
   // You must register the group before adding hotkeys to it
-	hotkeys.registerGroup({
+  log("Registering hotkeys group.");
+	Hotkeys.registerGroup({
 		name: `${MODULE_ID}.change-elevation-group`, // <- Must be unique
-		label: 'Elevation Ruler Change Elevation Keys',
-		description: 'Keys to increment and decrement elevation while using the ruler.' // <-- Optional
+		label: 'Elevation Ruler',
+		description: 'Keys to increase and decrease elevation while using the ruler.' // <-- Optional
 	});
 
-  hotkeys.registerShortcut({
+  log("Registering increment elevation hotkey.");
+  Hotkeys.registerShortcut({
 		name: `${MODULE_ID}.increment-elevation-hotkey`, // <- Must be unique
-		label: 'Increment Elevation for Ruler',
+		label: 'Increment',
+                repeat: true, // Let the user hold down the key to increase repeatedly.
 		group: `${MODULE_ID}.change-elevation-group`,
-		get: () => game.settings.get(MODULE_ID, 'increment-elevation-hotkey'),
-		set: async value => await game.settings.set(MODULE_ID, 'increment-elevation-hotkey', value),
-		default: () => { return { key: hotkeys.keys.KeyQ, alt: false, ctrl: false, shift: false }; },
+//		get: () => game.settings.get(MODULE_ID, `increment-elevation-hotkey`),
+//		set: async value => await game.settings.set(MODULE_ID, `increment-elevation-hotkey`, "ArrowUp"),
+		default: () => { return { key: Hotkeys.keys.BracketLeft, alt: false, ctrl: false, shift: false }; },
 		onKeyDown: self => { console.log('You hit my custom increment-elevation-hotkey!') },
 	}); 
-
-  hotkeys.registerShortcut({
+  
+  log("Registering decrement elevation hotkey.");
+  Hotkeys.registerShortcut({
 		name: `${MODULE_ID}.decrement-elevation-hotkey`, // <- Must be unique
-		label: 'Increment Elevation for Ruler',
+		label: 'Decrement',
+                repeat: true,
 		group: `${MODULE_ID}.change-elevation-group`,
-		get: () => game.settings.get(MODULE_ID, 'decrement-elevation-hotkey'),
-		set: async value => await game.settings.set(MODULE_ID, 'decrement-elevation-hotkey', value),
-		default: () => { return { key: hotkeys.keys.KeyQ, alt: false, ctrl: false, shift: false }; },
+//		get: () => game.settings.get(MODULE_ID, `${MODULE_ID}.decrement-elevation-hotkey`),
+//		set: async value => await game.settings.set(MODULE_ID, `${MODULE_ID}.decrement-elevation-hotkey`, "ArrowDown"),
+		default: () => { return { key: Hotkeys.keys.BracketRight, alt: false, ctrl: false, shift: false }; },
 		onKeyDown: self => { console.log('You hit my custom decrement-elevation-hotkey!') },
 	}); 
 
