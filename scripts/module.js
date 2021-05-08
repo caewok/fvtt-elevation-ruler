9914959050e515a5a56d4a6a7166ec15fbfea720
Michael Enion <caewok_2000@yahoo.com>
2021-05-08 06:15:14 -0700

    Add setting to select distance measure type.
    3 options, based on dnd5e choices: 
    - city block (add vertical to horizontal)
    - 5/10/5 (modified city block, doubling vertical every two
    - euclidean (mathematical distance, rounded)
    
    Also add basic log function.

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,28 @@
-Hooks.once('init', async function() {
+import { registerSettings } from "./settings.js";
 
+const MODULE_ID = 'elevation-ruler';
+const FORCE_DEBUG = true; // used for logging before dev mode is set up
+
+
+export function log(...args) {
+  try {
+    const isDebugging = window.DEV?.getPackageDebugValue(CONSTANTS.MODULE_ID);
+    console.log(MODULE_ID, '|', `isDebugging: ${isDebugging}.`);
+
+    if (FORCE_DEBUG || isDebugging) {
+      console.log(MODULE_ID, '|', ...args);
+    }
+  } catch (e) {}
+}
+
+
+Hooks.once('init', async function() {
+  console.log("Initializing Elevation Ruler Options.");
+  registerSettings();
 });
 
 Hooks.once('ready', async function() {
 
 });
+
+
