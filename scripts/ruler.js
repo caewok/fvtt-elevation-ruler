e4f49f2d0971ecbec8b75254b4960ff54abaa16b
Michael Enion <caewok_2000@yahoo.com>
2021-05-08 08:32:23 -0700

    Add framework libwrapper wraps for Ruler class.
    Basic framework for methods we might wrap or override:
    - constructor (silently fails to wrap)
    - measure
    - move token
    - clear
    - update
    - add waypoint

--- scripts/ruler.js
+++ scripts/ruler.js
@@ -8,6 +8,7 @@ import { log } from "./module.js";
  * Display current elevation change and change at each waypoint.
  */
  
+// wrapping the constructor appears not to work.
 export function elevationRulerConstructor(wrapped, ...args) {
   console.log("Elevation Ruler|wrapper elevationRulerConstructor called.");
   return wrapped(...args);
@@ -43,7 +44,38 @@ export function elevationRulerConstructor(wrapped, ...args) {
   //return result;
 }
 
+
+// will need to update measuring to account for elevation
 export function elevationRulerMeasure(wrapped, ...args) {
-  console.log("Elevation Ruler|we are measuring!");
+  log("we are measuring!", this);
   return wrapped(...args);
 }
+
+// moveToken should modify token elevation 
+export function elevationRulerMoveToken(wrapped, ...args) {
+  log("we are moving!", this);
+  return wrapped(...args);
+}
+
+// clear should reset elevation info
+export function elevationRulerClear(wrapped, ...args) {
+  log("we are clearing!", this);
+  return wrapped(...args);
+}
+
+// update will need to transfer relevant elevation data (probably?)
+export function elevationRulerUpdate(wrapped, ...args) {
+  log("we are updating!", this);
+  return wrapped(...args);
+}
+
+// adding waypoint should also add elevation info
+export function elevationRulerAddWaypoint(wrapped, ...args) {
+  log("adding waypoint!", this);
+  return wrapped(...args);
+}
+
+
+
+
+
