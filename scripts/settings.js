bf6ced6a883a7c2bb4694b490d845335cb2fabda
Michael Enion <caewok_2000@yahoo.com>
2021-05-22 13:27:33 -0700

    Remove measurement method setting
    Unnecessary, as the code can rely on the system diagonal measurement method.

--- scripts/settings.js
+++ scripts/settings.js
@@ -7,27 +7,6 @@ export function getSetting(settingName) {
 
 
 export function registerSettings() {
-  log("Registering measurement setting.");
-  game.settings.register(MODULE_ID, 'measurement-method', {
-    name: 'Measurement method',
-    hint: 
-`Change in elevation plus change horizontally is always considered diagonal movement. 
-| CityBlock: Add full vertical distance to the full horizontal distance moved. 
-| 5/10/5: Count every second unit of vertical movement as double.
-| Euclidean: Add exact mathematical distance of the diagonal move, rounded.
-`,
-    scope: 'world',
-    config: true,
-    type: String,
-    choices: {
-      'CityBlock': 'City Block',
-      'DnDAlt': '5/10/5 (Dnd5e alternative)',
-      'Euclidean': 'Euclidean'
-    },
-    default: 'CityBlock',
-  });
- 
-
   log("Registering hotkey menu.");
   game.settings.registerMenu(MODULE_ID, "elevationRulerHotkeyMenu", {
 	  name: 'Hotkeys Settings',
