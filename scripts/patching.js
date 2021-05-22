66e881783fa0c035961a46acaa9acde750d07321
Michael Enion <caewok_2000@yahoo.com>
2021-05-22 10:55:59 -0700

    Wrap animateToken to update elevation
    - Remove old wrap of moveToken
    - Update the token elevation along each step

--- scripts/patching.js
+++ scripts/patching.js
@@ -2,19 +2,22 @@ import { MODULE_ID, log } from "./module.js";
 import { elevationRulerConstructSegmentDistanceRay, 
          elevationRulerGetSegmentLabel,
          
-         elevationRulerMoveToken,
          elevationRulerClear,
          elevationRulerAddWaypoint,
-         elevationRulerRemoveWaypoint } from "./ruler.js";
+         elevationRulerRemoveWaypoint,
+         
+         elevationRulerAnimateToken } from "./ruler.js";
 
 export function registerRuler() {
 
-  // measuring functions
+  // measuring methods
   libWrapper.register(MODULE_ID, 'Ruler.prototype.constructSegmentDistanceRay', elevationRulerConstructSegmentDistanceRay, 'WRAPPER');
   libWrapper.register(MODULE_ID, 'Ruler.prototype.getSegmentLabel', elevationRulerGetSegmentLabel, 'WRAPPER');
 
-  // other functions
-  libWrapper.register(MODULE_ID, 'Ruler.prototype.moveToken', elevationRulerMoveToken, 'WRAPPER');
+  // move token methods
+  libWrapper.register(MODULE_ID, 'Ruler.prototype.animateToken', elevationRulerAnimateToken, 'WRAPPER');
+  
+  // other methods
   libWrapper.register(MODULE_ID, 'Ruler.prototype.clear', elevationRulerClear, 'WRAPPER');
   libWrapper.register(MODULE_ID, 'Ruler.prototype._addWaypoint', elevationRulerAddWaypoint, 'WRAPPER');
   libWrapper.register(MODULE_ID, 'Ruler.prototype._removeWaypoint', elevationRulerRemoveWaypoint, 'WRAPPER');
