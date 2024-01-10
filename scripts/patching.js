303fd6dba85b1742c8d43b5e6c584f0c89e7122b
Michael Enion <caewok_2000@yahoo.com>
2024-01-10 08:22:38 -0800

    Remove unused PlaceableObject patch

--- scripts/patching.js
+++ scripts/patching.js
@@ -8,7 +8,6 @@ import { MODULES_ACTIVE } from "./const.js";
 import { PATCHES as PATCHES_Ruler } from "./Ruler.js";
 import { PATCHES as PATCHES_Token } from "./Token.js";
 import { PATCHES as PATCHES_GridLayer } from "./GridLayer.js";
-import { PATCHES as PATCHES_PlaceableObject } from "./PlaceableObject.js";
 import { PATCHES as PATCHES_ClientKeybindings } from "./ClientKeybindings.js";
 import { PATCHES as PATCHES_BaseGrid } from "./BaseGrid.js";
 import { PATCHES as PATCHES_HexagonalGrid } from "./HexagonalGrid.js";
@@ -24,7 +23,6 @@ const PATCHES = {
   GridLayer: PATCHES_GridLayer,
   HexagonalGrid: PATCHES_HexagonalGrid,
   Ruler: PATCHES_Ruler,
-  PlaceableObject: PATCHES_PlaceableObject,
   Token: PATCHES_Token,
   Settings: PATCHES_Settings
 };
