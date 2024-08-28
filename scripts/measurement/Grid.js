e904b6503f5a547ace29830037e01af81d99eb4f
Michael Enion <caewok_2000@yahoo.com>
2024-08-28 09:31:01 -0700

    🐛 fix|Measurement|Gridless offset using pixel, not grid coordinates

--- scripts/measurement/Grid.js
+++ scripts/measurement/Grid.js
@@ -327,7 +327,7 @@ function _measurePath(wrapped, waypoints, { cost }, result) {
   cost ??= (prevOffset, currOffset, offsetDistance) => offsetDistance;
   switch ( canvas.grid.type ) {
     case CONST.GRID_TYPES.GRIDLESS:
-      offsetDistanceFn = Point3d.distanceBetween;
+      offsetDistanceFn = (a, b) => CONFIG.GeometryLib.utils.pixelsToGridUnits(Point3d.distanceBetween(a, b));
       break;
     case CONST.GRID_TYPES.SQUARE:
       offsetDistanceFn = singleOffsetSquareDistanceFn(diagonals);
