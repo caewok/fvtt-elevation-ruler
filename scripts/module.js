6191ba0ba03e53b962e99d7b4c0cddf5fd506b81
Michael Enion <caewok_2000@yahoo.com>
2026-02-21 13:58:36 -0800

    💡 refactor|WebGPU|Remove unused methods or those duplicated in worker

--- scripts/module.js
+++ scripts/module.js
@@ -33,7 +33,7 @@ import { GriddedCollisionPathfinder, worldBuilderGriddedCollision } from "./path
 import { ClockwiseSweepPathfinder, worldBuilderClockwise } from "./pathfinding/ClockwiseSweepPathfinding.js";
 
 // WebGPU pathfinding
-import { Terrain, WebGPUPathfinderWithFakeWorker, WebGPUPathfinder, GPUPathfinder } from "./pathfinding/WebGPUPathfinding.js";
+import { WebGPUPathfinder } from "./pathfinding/WebGPUPathfinding.js";
 
 // Scene graph
 import { EdgeGraph } from "./EdgeGraph.js";
@@ -58,7 +58,7 @@ import "./tests/EdgeGraph.test.js";
 
 Hooks.once("init", function() {
   // Test for WebGPU device.
-  GPUPathfinder.initializeDevice(); // Async.
+  if ( WebGPUPathfinder.supportsWebGPU ) WebGPUPathfinder.initialize(); // Async.
 
   // Configuration
   CONFIG[MODULE_ID] = {
@@ -143,9 +143,7 @@ Hooks.once("init", function() {
       AStarGraph,
       GraphingPathfinder,
 
-      Terrain,
       WebGPUPathfinder,
-      WebGPUPathfinderWithFakeWorker,
 
       GriddedCollisionPathfinder,
       worldBuilderGriddedCollision,
