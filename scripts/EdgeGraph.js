6f64b73d83fa64e82553d152d61dab676241fc23
Michael Enion <caewok_2000@yahoo.com>
2026-03-10 04:07:57 -0700

    Fix undefined error in EdgeGraph for tokenBlocks test

--- scripts/EdgeGraph.js
+++ scripts/EdgeGraph.js
@@ -815,14 +815,14 @@ export class EdgeGraph {
    * @returns {boolean} True if blocked
    */
   static tokenBlocksSegment(a, b, moveToken) {
-    // NOTE: rayIntersectionConstrained will return false if a.z is at the edge of the token.
+    // NOTE: rayIntersection will return false if a.z is at the edge of the token.
     // For example, if a.z === 0, tokens at elevation 0 will not block.
     using dir = b.subtract(a);
     for ( const token of canvas.tokens.placeables ) {
       const geom = token[GEOMETRY_LIB_ID][GEOMETRY_ID];
       if ( !geom ) continue;
       if ( !HalfEdge.tokenBlocks(token, moveToken) ) continue;
-      const ix = geom.rayIntersectionConstrained(a, dir);
+      const ix = geom.rayIntersection(a, dir);
       if ( ix ) return true;
     }
     return false;
